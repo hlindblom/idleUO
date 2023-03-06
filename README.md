cb560720b1d3f3bd6cfad3cc67bfc393b6866f57
Holden Lindblom <holden.lindblom@gmail.com>
2023-03-06 01:29:57 -0500

    major changes

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ideal Fighter
+
+## Version 1
+> introducing UO Idle fighter v.1. The idea of the game is to run similar to other ideal games, but in the setting of Ultima Online. 
+
+### Current Plan
+1. get base game functional
+2. add class system & armor
---
+++ data.js
@@ -0,0 +1,105 @@
+window.data = {
+  playerName: 'Balthasar',
+  lvl: 0, 
+  xp: 0,
+  totalCPS: 0,
+  fightRetreat: 'Fight',
+  enemies: [
+    {
+      id: 'rats',
+      price: 10,
+      basePrice: 10,
+      unlocked: false,
+      cps: 1,
+      qty: 0
+    },
+    {
+      id: 'mongbats',
+      price: 50,
+      basePrice: 50,
+      unlocked: false,
+      cps: 2,
+      qty: 0
+    },
+    {
+      id: 'headless',
+      price: 100,
+      basePrice: 100,
+      unlocked: false,
+      cps: 5,
+      qty: 0
+    },
+    {
+      id: 'harpies',
+      price: 500,
+      basePrice: 500,
+      unlocked: false,
+      cps: 10,
+      qty: 0
+    },
+    {
+      id: 'skeletons',
+      price: 1000,
+      basePrice: 1000,
+      unlocked: false,
+      cps: 20,
+      qty: 0
+    },
+    {
+      id: 'orcs',
+      price: 5000,
+      basePrice: 5000,
+      unlocked: false,
+      cps: 50,
+      qty: 0
+    },
+    {
+      id: 'ogres',
+      price: 10000,
+      basePrice: 10000,
+      unlocked: false,
+      cps: 75,
+      qty: 0
+    },
+    {
+      id: 'ophidian_knights',
+      price: 50000,
+      basePrice: 50000,
+      unlocked: false,
+      cps: 100,
+      qty: 0
+    },
+    {
+      id: 'fire_ants',
+      price: 100000,
+      basePrice: 100000,
+      unlocked: false,
+      cps: 200,
+      qty: 0
+    },
+    {
+      id: 'daemons',
+      price: 500000,
+      basePrice: 500000,
+      unlocked: false,
+      cps: 500,
+      qty: 0
+    },
+    {
+      id: 'demon_knights',
+      price: 1000000,
+      basePrice: 1000000,
+      unlocked: false,
+      cps: 1000,
+      qty: 0
+    },
+    {
+      id: 'rust_dragons',
+      price: 5000000,
+      basePrice: 5000000,
+      unlocked: false,
+      cps: 2000,
+      qty: 0
+    }
+  ]
+};
